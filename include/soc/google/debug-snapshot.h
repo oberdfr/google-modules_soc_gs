f809fd6bca86ac2cb9e45cbb6495ba4cadb788ef
Dyneteve <dyneteve@hentaios.com>
2024-03-28 18:16:03 +0000

    Revert "soc/google: debug-kinfo: backup debug-snapshot info."
    This will lead to pixel loader crashing on GS101/GS201 while we could guard
    this code we really do not need this information dumped.
    
    This reverts commit 5444ec57afe98482ae031fd8bb8d4210d46fd6c9.
    
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- include/soc/google/debug-snapshot.h
+++ include/soc/google/debug-snapshot.h
@@ -19,9 +19,6 @@
 #if IS_ENABLED(CONFIG_DEBUG_SNAPSHOT)
 #include <linux/sched/clock.h>
 
-#define DSS_FREQ_MAX_SIZE		SZ_32
-#define DSS_FREQ_MAX_NAME_SIZE		SZ_8
-
 struct clk;
 struct clk_hw;
 struct exynos_tmu_data;
@@ -79,8 +76,6 @@ extern unsigned int dbg_snapshot_get_core_ehld_stat(unsigned int cpu);
 
 /* debug-snapshot-log functions */
 extern int dbg_snapshot_get_freq_idx(const char *name);
-extern void dbg_snapshot_get_freq_name(char (*freq_names)[DSS_FREQ_MAX_NAME_SIZE]);
-extern unsigned int dbg_snapshot_get_freq_size(void);
 extern void *dbg_snapshot_get_suspend_diag(void);
 
 #define dbg_snapshot_get_timestamp()	local_clock()
@@ -197,8 +192,6 @@ static inline void dbg_snapshot_spin_func(void)
 #define dbg_snapshot_stop_all_cpus()		(-1)
 
 #define dbg_snapshot_get_freq_idx(a)		(-1)
-#define dbg_snapshot_get_freq_name(a)		(0)
-#define dbg_snapshot_get_freq_size()		(0)
 
 #define dss_extern_get_log_by_cpu(item)					\
 static inline long dss_get_len_##item##_log(void) {			\
